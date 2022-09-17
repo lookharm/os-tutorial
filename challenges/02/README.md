2c945e1de7fd34d6659eb7eff63c1173151affad
woradorn.l <woradorn.laon@gmail.com>
2022-09-17 23:16:16 +0700

    Add challenges/02

---
+++ challenges/02/README.md
@@ -0,0 +1,4 @@
+# 02
+
+Write a boot sector program that prints "HELLO".
+[solution](./hello.asm)
---
+++ challenges/02/hello.asm
@@ -0,0 +1,20 @@
+mov ah, 0x0e
+
+mov al, 'H'
+int 0x10
+
+mov al, 'E'
+int 0x10
+
+mov al, 'L'
+int 0x10
+
+mov al, 'L'
+int 0x10
+
+mov al, 'O'
+int 0x10
+
+jmp $
+times 510-($-$$) db 0x00
+dw 0xaa55
